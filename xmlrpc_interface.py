3b4b92dfb64b2b9259477c7ce50ca7658b5a1396
Cristiano Pimenta <cgpimenta@dcc.ufmg.br>
2019-03-07 15:50:39 -0300

    Removed unused code

--- xmlrpc_interface.py
+++ xmlrpc_interface.py
@@ -13,18 +13,21 @@ from hashlib import md5
 
 stop_server = False
 
+
 def eval_dags(inputs: multiprocessing.Queue, outputs: multiprocessing.Queue):
     while True:
         try:
             ind_id, ind_dag, filename, log_info = inputs.get(block=False)
             log_info['size'] = len(ind_dag)
             log_info['eval_start'] = time.time()
-            errors, id = eval.safe_dag_eval(dag=ind_dag, filename=filename, dag_id=ind_id)
+            errors, id = eval.safe_dag_eval(
+                dag=ind_dag, filename=filename, dag_id=ind_id)
             assert ind_id == id
             log_info['eval_end'] = time.time()
             outputs.put((ind_id, errors, ind_dag, log_info))
         except Exception:
             time.sleep(1)
+
 
 class DagEvalServer:
 
@@ -37,7 +40,8 @@ class DagEvalServer:
         self.gen_number = 0
         self.inputs = multiprocessing.Queue()
         self.outputs = multiprocessing.Queue()
-        self.processes = [multiprocessing.Process(target=eval_dags, args=(self.inputs, self.outputs)) for _ in range(n_cpus)]
+        self.processes = [multiprocessing.Process(target=eval_dags, args=(
+            self.inputs, self.outputs)) for _ in range(n_cpus)]
         for p in self.processes:
             p.start()
         self.log = []
@@ -61,7 +65,8 @@ class DagEvalServer:
 
         try:
             while True:
-                ind_id, ind_eval, ind_dag, log_info = self.outputs.get(block=False)
+                ind_id, ind_eval, ind_dag, log_info = self.outputs.get(
+                    block=False)
                 kappa = ind_eval[0] if ind_eval else 'error'
                 std = ind_eval[1] if ind_eval else 'error'
                 eval_time = ind_eval[2] if ind_eval else 'error'
@@ -87,31 +92,6 @@ class DagEvalServer:
     def kill_workers(self):
         for p in self.processes:
             p.terminate()
-
-
-    def eval(self, json_string, datafile):
-        """
-        Evaluates all dags described by the json_string on the dataset with name datafile.
-        :param json_string: JSON string containing the list of dags to evaluate.
-        :param datafile: The dataset to evaluate the dags on.
-        :return: List of tuples with the scores of each dag on the dataset
-        """
-        dags = json.loads(json_string)
-        for dag in dags:
-            self.submit(dag, datafile)
-
-        ret = []
-        while len(ret) < len(dags):
-            r = self.get_evaluated()
-            if not r:
-                continue
-            ret.append(r)
-
-        ret = [r[1] for r in sorted(ret, key=lambda x: x[0])]
-        log_file = os.path.join(self.log_path, 'log_%03d.json' % self.gen_number)
-        json.dump(list(zip(dags, ret)), open(log_file, 'w'), indent=1)
-        self.gen_number += 1
-        return ret
 
     def get_param_sets(self, datafile):
         """
