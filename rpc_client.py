cf801d5a585ada476927111f7bd33c6fe2d315b6
Cristiano Pimenta <cgpimenta@dcc.ufmg.br>
2019-03-11 15:35:52 -0300

    Added an argument to the submit method to receive a list of metrics

--- rpc_client.py
+++ rpc_client.py
@@ -8,8 +8,8 @@ class RPCClient(object):
         self.server_url = server_url
         self.server_proxy = ServerProxy(server_url)
 
-    def submit(self, candidate, dataset):
-        return self.server_proxy.submit(candidate, dataset)
+    def submit(self, candidate, dataset, metrics_list):
+        return self.server_proxy.submit(candidate, dataset, metrics_list)
 
     def get_evaluated(self):
         cand_id, metrics = json.loads(self.server_proxy.get_evaluated())
--- test_client.py
+++ test_client.py
@@ -4,13 +4,20 @@ import time
 
 from rpc_client import RPCClient
 
-
 client = RPCClient('http://localhost:8080')
+# client = RPCClient('http://automl.speed.dcc.ufmg.br:80')
 
 json_string =   '{"input": [[], "input", ["1:0"]], "1": [["1:0"], ["gaussianNB", {}], []]}'
 dataset = 'winequality-white.csv'
 
-cand_id = client.submit(json_string, dataset)
+metrics_list = [
+    {'metric': 'accuracy', 'args': {}, 'name': 'accuracy'},
+    {'metric': 'f1', 'args': {'average': 'micro'}, 'name': 'f1_micro'},
+    {'metric': 'f1', 'args': {'average': 'macro'}, 'name': 'f1_macro'},
+    {'metric': 'f1', 'args': {'average': 'weighted'}, 'name': 'f1_weighted'}
+]
+
+cand_id = client.submit(json_string, dataset, metrics_list)
 
 print('Candidate id:', cand_id)
 
@@ -20,4 +27,3 @@ results = client.get_evaluated()
 
 print('\nResults:')
 print(results)
-
